eaa8f049fb3ac611732c3cf4ae24b42eef3e84f0
rilwan99 <77623631+rilwan99@users.noreply.github.com>
2022-04-08 00:14:12 +0800

    Added some styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 
-import {PanelModule} from 'primeng/panel';
-import {TableModule} from 'primeng/table';
-import {ButtonModule} from 'primeng/button';
-import {DialogModule} from 'primeng/dialog';
-import {RatingModule} from 'primeng/rating';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { CardModule } from 'primeng/card';
+import { CarouselModule } from 'primeng/carousel';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DataViewModule } from 'primeng/dataview';
+import { DividerModule } from 'primeng/divider';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { PasswordModule } from 'primeng/password';
+import { RatingModule } from 'primeng/rating';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { SharedModule } from 'primeng/api';
+import { SliderModule } from 'primeng/slider';
+import { StepsModule } from 'primeng/steps';
+import { TabViewModule } from 'primeng/tabview';
+import { TableModule } from 'primeng/table';
+import { Toast, ToastModule } from 'primeng/toast';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TooltipModule } from 'primeng/tooltip';
+import { DialogModule } from 'primeng/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,21 +53,45 @@ import { AccessRightErrorComponent } from './access-right-error/access-right-err
     MainMenuComponent,
     SidebarComponent,
     BreadcrumbComponent,
-    AccessRightErrorComponent
+    AccessRightErrorComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BreadcrumbModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    ButtonModule,
+    CalendarModule,
+    CardModule,
+    CarouselModule,
+    CheckboxModule,
+    ColorPickerModule,
+    CommonModule,
+    ConfirmDialogModule,
+    DataViewModule,
+    DividerModule,
+    DropdownModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    PanelModule,
+    InputNumberModule,
+    InputTextModule,
+    MenuModule,
+    MenubarModule,
+    MultiSelectModule,
+    PasswordModule,
+    RatingModule,
+    SelectButtonModule,
+    SharedModule,
+    SliderModule,
+    StepsModule,
+    TabViewModule,
     TableModule,
-    ButtonModule,
+    ToastModule,
+    ToolbarModule,
+    TooltipModule,
     DialogModule,
-    RatingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
