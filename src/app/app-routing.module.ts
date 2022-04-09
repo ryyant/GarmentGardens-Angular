31e52713a1dc6fc492e3b3eedd773aca5b169f4a
ryyant <ryyant@hotmail.com>
2022-04-09 17:16:26 +0800

    Feat: UI Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { IndexComponent } from './index/index.component';
+import { AccessRightErrorComponent } from './access-right-error/access-right-error.component';
+import { CheckoutComponent } from './cashierOperation/checkout/checkout.component';
+import { VoidRefundComponent } from './cashierOperation/void-refund/void-refund.component';
+import { ViewMySaleTransactionsComponent } from './cashierOperation/view-my-sale-transactions/view-my-sale-transactions.component';
+import { CreateNewStaffComponent } from './systemAdministration/create-new-staff/create-new-staff.component';
+import { ViewAllStaffsComponent } from './systemAdministration/view-all-staffs/view-all-staffs.component';
+import { CreateNewProductComponent } from './systemAdministration/create-new-product/create-new-product.component';
+import { ViewAllProductsComponent } from './systemAdministration/view-all-products/view-all-products.component';
+import { ViewProductDetailsComponent } from './systemAdministration/view-product-details/view-product-details.component';
+import { UpdateProductComponent } from './systemAdministration/update-product/update-product.component';
+import { DeleteProductComponent } from './systemAdministration/delete-product/delete-product.component';
+import { ViewAllProductsPfComponent } from './systemAdministration/view-all-products-pf/view-all-products-pf.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/index', pathMatch: 'full' },
   { path: 'index', component: IndexComponent },
+  { path: 'accessRightError', component: AccessRightErrorComponent },
+  { path: 'cashierOperation/checkout', component: CheckoutComponent },
+  { path: 'cashierOperation/voidRefund', component: VoidRefundComponent },
+  { path: 'cashierOperation/viewMySaleTransactions', component: ViewMySaleTransactionsComponent },
+  { path: 'systemAdministration/createNewStaff', component: CreateNewStaffComponent },
+  { path: 'systemAdministration/viewAllStaffs', component: ViewAllStaffsComponent },
+  { path: 'systemAdministration/createNewProduct', component: CreateNewProductComponent },
+  { path: 'systemAdministration/viewAllProducts', component: ViewAllProductsComponent },
+  { path: 'systemAdministration/viewProductDetails', component: ViewProductDetailsComponent },
+  { path: 'systemAdministration/viewProductDetails/:productId', component: ViewProductDetailsComponent },
+  { path: 'systemAdministration/updateProduct', component: UpdateProductComponent },
+  { path: 'systemAdministration/updateProduct/:productId', component: UpdateProductComponent },
+  { path: 'systemAdministration/deleteProduct', component: DeleteProductComponent },
+  { path: 'systemAdministration/deleteProduct/:productId', component: DeleteProductComponent },
+  { path: 'systemAdministration/viewAllProductsPf', component: ViewAllProductsPfComponent },
 ];
 
 @NgModule({
--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {MenuItem} from 'primeng/api';
 
 import { SessionService } from '../services/session.service';
 
@@ -16,10 +17,36 @@ export class MainMenuComponent implements OnInit
 
   }
 
-  ngOnInit(): void {
+  items: MenuItem[] = [];
+
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'File',
+              items: [{
+                      label: 'New', 
+                      icon: 'pi pi-fw pi-plus',
+                      items: [
+                          {label: 'Project'
+                          , routerLink: ['/systemAdministration/viewAllProducts']},
+                          {label: 'Other'
+                          , routerLink: ['/pagename']},
+                      ]
+                  },
+                  {label: 'Open'},
+                  {label: 'Quit'}
+              ]
+          },
+          {
+              label: 'Edit',
+              icon: 'pi pi-fw pi-pencil',
+              items: [
+                  {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+                  {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+              ]
+          }
+      ];
   }
-
-
 
   userLogout(): void
 	{
