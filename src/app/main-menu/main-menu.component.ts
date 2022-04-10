4918eb8dc6dc7e159e011c08f500eb2cdb826cfa
ryyant <ryyant@hotmail.com>
2022-04-10 16:20:09 +0800

    Style: Menu/Header

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,58 +1,93 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {MenuItem} from 'primeng/api';
+import { MenuItem } from 'primeng/api';
 
 import { SessionService } from '../services/session.service';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
-  styleUrls: ['./main-menu.component.css']
+  styleUrls: ['./main-menu.component.css'],
 })
-export class MainMenuComponent implements OnInit
-{
-  constructor(private router: Router,
-              public sessionService: SessionService)
-  {
-
-  }
+export class MainMenuComponent implements OnInit {
+  constructor(private router: Router, public sessionService: SessionService) {}
 
   items: MenuItem[] = [];
 
   ngOnInit() {
-      this.items = [
+    this.items = [
+      {
+        label: 'Browse',
+        items: [
           {
-              label: 'File',
-              items: [{
-                      label: 'New', 
-                      icon: 'pi pi-fw pi-plus',
-                      items: [
-                          {label: 'Project'
-                          , routerLink: ['/systemAdministration/viewAllProducts']},
-                          {label: 'Other'
-                          , routerLink: ['/pagename']},
-                      ]
-                  },
-                  {label: 'Open'},
-                  {label: 'Quit'}
-              ]
+            label: 'Tops',
+            items: [
+              {
+                label: 'Products',
+                routerLink: ['/systemAdministration/viewAllProductsPf'],
+              },
+              { label: 'Other', routerLink: ['/pagename'] },
+            ],
           },
           {
-              label: 'Edit',
-              icon: 'pi pi-fw pi-pencil',
-              items: [
-                  {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-                  {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-              ]
-          }
-      ];
+            label: 'Bottoms',
+            items: [
+              {
+                label: 'Products',
+                routerLink: ['/systemAdministration/viewAllProducts'],
+              },
+              { label: 'Other', routerLink: ['/pagename'] },
+            ],
+          },
+          {
+            label: 'Shoes',
+            items: [
+              {
+                label: 'Products',
+                routerLink: ['/systemAdministration/viewAllProducts'],
+              },
+              { label: 'Other', routerLink: ['/pagename'] },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Customer',
+        items: [
+          {
+            label: 'Transactions',
+            routerLink: ['/systemAdministration/viewAllProducts'],
+          },
+          {
+            label: 'Cart',
+            routerLink: ['/systemAdministration/viewAllProducts'],
+          },
+          {
+            label: 'Disputes',
+            routerLink: ['/systemAdministration/viewAllProducts'],
+          },
+        ],
+      },
+      {
+        label: 'Seller',
+        items: [
+          {
+            label: 'Manage Products',
+            routerLink: ['/systemAdministration/viewAllProducts'],
+          },
+          {
+            label: 'Disputes',
+            routerLink: ['/systemAdministration/viewAllProducts'],
+          },
+        ],
+      },
+    ];
   }
 
-  userLogout(): void
-	{
-		this.sessionService.setIsLogin(false);
-		this.sessionService.setCurrentUser(null);
-		
-		this.router.navigate(["/index"]);
-	}
+  userLogout(): void {
+    this.sessionService.setIsLogin(false);
+    this.sessionService.setCurrentUser(null);
+
+    this.router.navigate(['/index']);
+  }
 }
