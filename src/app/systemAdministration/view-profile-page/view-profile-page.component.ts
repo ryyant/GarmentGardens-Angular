f8b6becf912ecddf9188ec7edaff11a5bff9d2c8
rilwan99 <77623631+rilwan99@users.noreply.github.com>
2022-04-12 00:33:12 +0800

    Date bug fix

--- src/app/systemAdministration/view-profile-page/view-profile-page.component.ts
+++ src/app/systemAdministration/view-profile-page/view-profile-page.component.ts
@@ -6,6 +6,7 @@ import { UserService } from '../../services/user.service';
 import { SelectItem, PrimeNGConfig } from 'primeng/api';
 import { SessionService } from '../../services/session.service';
 import { MessageService } from 'primeng/api';
+import { RoleEnum } from 'src/app/models/role-enum';
 
 @Component({
   selector: 'app-view-profile-page',
@@ -21,6 +22,11 @@ export class ViewProfilePageComponent implements OnInit {
   resultError: boolean;
   message: string | undefined;
 
+  yearRange: string = '1921:' + new Date().getFullYear();
+  maxDate: Date = new Date();
+
+  stateOptions: any[];
+
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
@@ -33,12 +39,26 @@ export class ViewProfilePageComponent implements OnInit {
 
     this.resultSuccess = false;
     this.resultError = false;
+    this.stateOptions = [
+      { label: 'Customer', value: 'CUSTOMER' },
+      { label: 'Seller', value: 'SELLER' },
+    ];
   }
 
   ngOnInit(): void {
     this.checkLogin();
-    console.log('Customer DOB::' + this.currUser.dateOfBirth);
-    console.log('Customer DOB Type ::' + typeof this.currUser.dateOfBirth);
+    let dateString = new String(this.currUser.dateOfBirth);
+    let updateString = dateString.slice(0, 10);
+    //console.log(updateString);
+    this.currUser.dateOfBirth = new Date(updateString);
+
+    //console.log('Customer Role Type ::' + typeof this.currUser.role);
+    //console.log(this.currUser.role);
+    // if (this.sessionService.getCurrentUser().role == 0) {
+    //   this.currUser.role = RoleEnum.CUSTOMER;
+    // } else {
+    //   this.currUser.role = RoleEnum.SELLER;
+    // }
   }
 
   updateProfile(updateProfileForm: NgForm) {
@@ -46,6 +66,8 @@ export class ViewProfilePageComponent implements OnInit {
     let tempUser: User = Object.assign({}, this.currUser);
     tempUser.dateOfBirth = undefined;
     tempUser.password = this.sessionService.getPassword();
+    console.log('Temp User role is ::' + tempUser.role);
+    console.log('Temp User role type is ::' + typeof tempUser.role);
 
     if (updateProfileForm.valid) {
       this.userService.updateUser(tempUser).subscribe({
