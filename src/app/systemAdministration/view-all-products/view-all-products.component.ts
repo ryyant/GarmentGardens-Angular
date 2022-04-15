ebf3b93e0754407f34b687fd1d2422747b39a45a
ryyant <ryyant@hotmail.com>
2022-04-15 15:28:07 +0800

    UI: Success Message

--- src/app/systemAdministration/view-all-products/view-all-products.component.ts
+++ src/app/systemAdministration/view-all-products/view-all-products.component.ts
@@ -12,98 +12,103 @@ import { PrimeNGConfig } from 'primeng/api';
 @Component({
   selector: 'app-view-all-products',
   templateUrl: './view-all-products.component.html',
-  styleUrls: ['./view-all-products.component.css']
+  styleUrls: ['./view-all-products.component.css'],
 })
-export class ViewAllProductsComponent implements OnInit 
-{
+export class ViewAllProductsComponent implements OnInit {
   products: Product[];
-	display: boolean;
-	productToView: Product;
-  invalidQty: boolean;
-  
-  sortOptions: SelectItem[] = [];
-
-  sortOrder: number = 0;
-  sortKey: string = "";
-  sortField: string = "";
-
+  display: boolean;
+  productToView: Product;
+  message: string = '';
   qtyToAdd: number = 0;
 
-  constructor(private router: Router,
+  sortOptions: SelectItem[] = [];
+  sortOrder: number = 0;
+  sortKey: string = '';
+  sortField: string = '';
+
+  constructor(
+    private router: Router,
     private activatedRoute: ActivatedRoute,
     public sessionService: SessionService,
     private productService: ProductService,
     private cartService: CartService,
-    private primengConfig: PrimeNGConfig)
-  {
+    private primengConfig: PrimeNGConfig
+  ) {
     this.products = new Array();
     this.display = false;
-		this.productToView = new Product();
-    this.invalidQty = false;
+    this.productToView = new Product();
+    this.message = '';
   }
 
-  ngOnInit(): void
-  {
+  ngOnInit(): void {
     this.productService.getProducts().subscribe({
-      next:(response)=>{ 
+      next: (response) => {
         this.products = response;
       },
-      error:(error)=>{
+      error: (error) => {
         console.log('********** ViewAllProductsComponent.ts: ' + error);
-      }
+      },
     });
 
     this.sortOptions = [
       { label: 'Price High to Low', value: '!unitPrice' },
-      { label: 'Price Low to High', value: 'unitPrice' }
+      { label: 'Price Low to High', value: 'unitPrice' },
     ];
 
     this.primengConfig.ripple = true;
   }
 
-  showDialog(productToView: Product)
-	{
+  showDialog(productToView: Product) {
     this.display = true;
     this.productToView = productToView;
   }
 
-  viewProductDetails()
-  {
-    this.router.navigate(["/systemAdministration/viewProductDetails/" + this.productToView.productId]);
-  }
-
-  addToCart()
-	{
-    if (this.qtyToAdd == 0 || this.qtyToAdd > this.productToView.quantityOnHand ) {
-      console.log("Invalid quantity input!");
-      this.invalidQty = true;
-    } else {
-      this.invalidQty = false;
-      // CALL SERVICE HERE, TAKE IN qtyToAdd and productToView
-      this.cartService.addToCart(this.productToView, this.qtyToAdd).subscribe({
-        next: (response) => {
-        },
-        error: (error) => {
-          console.log('********** Add to cart: ' + error);
-        }
-      });
+  viewProductDetails() {
+    if (this.productToView != undefined) {
+      this.router.navigate([
+        '/systemAdministration/viewProductDetails/' +
+          this.productToView.productId,
+      ]);
     }
   }
 
-  onSortChange(event: { value: any; }) {
+  addToCart() {
+    if (
+      this.productToView != null &&
+      this.productToView.quantityOnHand != null
+    ) {
+      if (
+        this.qtyToAdd == 0 ||
+        this.qtyToAdd > this.productToView.quantityOnHand
+      ) {
+        console.log('Invalid quantity input!');
+        this.message = 'Error';
+      } else {
+        this.message = "";
+        // CALL SERVICE HERE, TAKE IN qtyToAdd and productToView
+        this.cartService
+          .addToCart(this.productToView, this.qtyToAdd)
+          .subscribe({
+            next: (response) => {
+              console.log("added to cart!");
+              this.message = 'Success';
+            },
+            error: (error) => {
+              console.log('********** Add to cart: ' + error);
+            },
+          });
+      }
+    }
+  }
+
+  onSortChange(event: { value: any }) {
     let value = event.value;
-
-    console.log("Sorting");
-    console.log(value);
     if (value.indexOf('!') === 0) {
-        this.sortOrder = -1;
-        this.sortField = value.substring(1, value.length);
+      this.sortOrder = -1;
+      this.sortField = value.substring(1, value.length);
+    } else {
+      this.sortOrder = 1;
+      this.sortField = value;
     }
-    else {
-        this.sortOrder = 1;
-        this.sortField = value;
-    }
+  }
 }
-
-}
-
--- src/app/view-product-by-category/view-product-by-category.component.ts
+++ src/app/view-product-by-category/view-product-by-category.component.ts
@@ -4,7 +4,8 @@ import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { SessionService } from '../services/session.service';
 import { ProductService } from '../services/product.service';
 import { CategoryService } from '../services/category.service';
-import { Product } from '../models/product'
+import { CartService } from '../services/cart.service';
+import { Product } from '../models/product';
 
 import { SelectItem } from 'primeng/api';
 import { PrimeNGConfig } from 'primeng/api';
@@ -13,109 +14,138 @@ import { Category } from '../models/category';
 @Component({
   selector: 'app-view-product-by-category',
   templateUrl: './view-product-by-category.component.html',
-  styleUrls: ['./view-product-by-category.component.css']
+  styleUrls: ['./view-product-by-category.component.css'],
 })
 export class ViewProductByCategoryComponent implements OnInit {
   products: Product[];
-	display: boolean;
-	productToView: Product;
-  
+  display: boolean;
+  productToView: Product;
+  message: string = '';
+  qtyToAdd: number = 0;
+
   categoryId: string | null;
   viewCategory: Category | null;
 
   sortOptions: SelectItem[] = [];
-
   sortOrder: number = 0;
-  sortKey: string = "";
-  sortField: string = "";
+  sortKey: string = '';
+  sortField: string = '';
 
-  qtyToAdd: number = 0;
-
-  constructor(private router: Router,
+  constructor(
+    private router: Router,
     private activatedRoute: ActivatedRoute,
     public sessionService: SessionService,
     private categoryService: CategoryService,
     private productService: ProductService,
-    private primengConfig: PrimeNGConfig)
-  {
+    private cartService: CartService,
+    private primengConfig: PrimeNGConfig
+  ) {
     this.products = new Array();
     this.categoryId = this.activatedRoute.snapshot.paramMap.get('categoryId');
     this.display = false;
-		this.productToView = new Product();
+    this.productToView = new Product();
     this.viewCategory = new Category();
   }
 
-  ngOnInit(): void
-  {
+  ngOnInit(): void {
     this.renderProducts();
 
     // IMPORTANT FOR SAME ROUTE, DYNAMIC CHANGE
-    this.router.events.subscribe(event =>{
-      if (event instanceof NavigationEnd){
-        this.categoryId = this.activatedRoute.snapshot.paramMap.get('categoryId');
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.categoryId =
+          this.activatedRoute.snapshot.paramMap.get('categoryId');
         this.renderProducts();
-        console.log("Browsing Category: " + this.categoryId);
+        console.log('Browsing Category: ' + this.categoryId);
       }
-   })
+    });
 
-   this.sortOptions = [
-    { label: 'Price High to Low', value: '!unitPrice' },
-    { label: 'Price Low to High', value: 'unitPrice' }
-  ];
+    this.sortOptions = [
+      { label: 'Price High to Low', value: '!unitPrice' },
+      { label: 'Price Low to High', value: 'unitPrice' },
+    ];
 
     this.primengConfig.ripple = true;
   }
 
-  renderProducts()
-  {
-    if(this.categoryId != null) {
-      this.categoryService.getCategoryByCategoryId(parseInt(this.categoryId)).subscribe({
-        next:(response)=>{ 
-          this.viewCategory = response;
-        },
-        error:(error)=>{
-          console.log('********** ViewAllProductsComponent.ts: ' + error);
-        }
-      });
+  renderProducts() {
+    if (this.categoryId != null) {
+      this.categoryService
+        .getCategoryByCategoryId(parseInt(this.categoryId))
+        .subscribe({
+          next: (response) => {
+            this.viewCategory = response;
+          },
+          error: (error) => {
+            console.log('********** ViewAllProductsComponent.ts: ' + error);
+          },
+        });
 
-      this.productService.getFilteredProducts(parseInt(this.categoryId)).subscribe({
-        next:(response)=>{ 
-          this.products = response;
-        },
-        error:(error)=>{
-          console.log('********** ViewAllProductsComponent.ts: ' + error);
-        }
-      });
+      this.productService
+        .getFilteredProducts(parseInt(this.categoryId))
+        .subscribe({
+          next: (response) => {
+            this.products = response;
+          },
+          error: (error) => {
+            console.log('********** ViewAllProductsComponent.ts: ' + error);
+          },
+        });
     }
   }
 
-  showDialog(productToView: Product)
-	{
+  showDialog(productToView: Product) {
     this.display = true;
     this.productToView = productToView;
   }
 
-  viewProductDetails()
-  {
-    this.router.navigate(["/systemAdministration/viewProductDetails/" + this.productToView.productId]);
+  viewProductDetails() {
+    if (this.productToView != undefined) {
+      this.router.navigate([
+        '/systemAdministration/viewProductDetails/' +
+          this.productToView.productId,
+      ]);
+    }
   }
 
-  addToCart()
-	{
-    // CALL SERVICE HERE, TAKE IN qtyToAdd
+  addToCart() {
+    if (
+      this.productToView != null &&
+      this.productToView.quantityOnHand != null
+    ) {
+      if (
+        this.qtyToAdd == 0 ||
+        this.qtyToAdd > this.productToView.quantityOnHand
+      ) {
+        console.log('Invalid quantity input!');
+        this.message = 'Error';
+      } else {
+        this.message = '';
+        // CALL SERVICE HERE, TAKE IN qtyToAdd and productToView
+        this.cartService
+          .addToCart(this.productToView, this.qtyToAdd)
+          .subscribe({
+            next: (response) => {
+              console.log('added to cart!');
+              this.message = 'Success';
+            },
+            error: (error) => {
+              console.log('********** Add to cart: ' + error);
+            },
+          });
+      }
+    }
   }
 
-  onSortChange(event: { value: any; }) {
+  onSortChange(event: { value: any }) {
     let value = event.value;
 
     if (value.indexOf('!') === 0) {
-        this.sortOrder = -1;
-        this.sortField = value.substring(1, value.length);
+      this.sortOrder = -1;
+      this.sortField = value.substring(1, value.length);
+    } else {
+      this.sortOrder = 1;
+      this.sortField = value;
     }
-    else {
-        this.sortOrder = 1;
-        this.sortField = value;
-    }
-}
-
+  }
 }
