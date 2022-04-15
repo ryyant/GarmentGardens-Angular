3d66e3e7a745459c2e528137f606ad7a98fb2b6e
ryyant <ryyant@hotmail.com>
2022-04-15 16:07:54 +0800

    Feat: interval ads

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  ads: number[] = [];
 
-  constructor() { }
+  constructor() { 
+    this.ads[0] = 0;
+    this.ads[1] = 1;
+    this.ads[2] = 2;
+  }
 
   ngOnInit(): void {
+    setInterval(() => this.randomizeAds(), 5000);
+  }
+
+  randomizeAds(): void {
+    let counter = 0;
+
+    while (counter < 4) {
+      let rand = this.getRandomInt(11);
+      if (!this.ads.includes(rand)) {
+        this.ads[counter] = rand;
+        counter++;
+      }
+    }
+  }
+
+  getRandomInt(max: number): number {
+    return Math.floor(Math.random() * max);
   }
 
 }
