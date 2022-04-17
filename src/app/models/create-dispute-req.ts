c5afc1aabdb24d9792225529e5ba8693a0f40a9f
Wong Deshun <77624433+Deshun99@users.noreply.github.com>
2022-04-17 09:18:12 +0800

    dispute work liao
    omg finally

--- src/app/models/create-dispute-req.ts
+++ src/app/models/create-dispute-req.ts
@@ -5,12 +5,16 @@ export class CreateDisputeReq {
   password: string | undefined;
   dispute: Dispute | undefined;
   orderId : string | undefined;
+  title: string | undefined;
+  description: string | undefined;
 
-  constructor(username?: string, password?: string, dispute?: Dispute, orderId?: string)
+  constructor(username?: string, password?: string, dispute?: Dispute, orderId?: string, title?: string, description?: string)
   {		
     this.username = username;
     this.password = password;
     this.dispute = dispute;
     this.orderId = orderId;
+    this.title = title;
+    this.description = description;
   }
 }
