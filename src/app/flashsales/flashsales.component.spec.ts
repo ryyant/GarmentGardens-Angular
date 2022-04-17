30298a408e95b7d68f2be886292efe853a9eec33
brennan-lee <brennan.lee@u.nus.edu>
2022-04-17 11:06:24 +0800

    flash sale component

---
+++ src/app/flashsales/flashsales.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FlashsalesComponent } from './flashsales.component';
+
+describe('FlashsalesComponent', () => {
+  let component: FlashsalesComponent;
+  let fixture: ComponentFixture<FlashsalesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FlashsalesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FlashsalesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/flashsales/flashsales.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, Output, Input, EventEmitter, ChangeDetectorRef } from '@angular/core';
+import { Subscription, Observable, timer } from 'rxjs';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-flashsales',
+  templateUrl: './flashsales.component.html',
+  styleUrls: ['./flashsales.component.css']
+})
+export class FlashsalesComponent implements OnInit {
+  private subscription: Subscription;
+  @Output() TimerExpired: EventEmitter<any> = new EventEmitter();
+  
+      @Input() SearchDate: moment.Moment = moment();
+  @Input() ElapsTime: number = 20;
+  
+  searchEndDate: moment.Moment;
+  remainingTime: number;
+  minutes: number;
+  seconds: number;
+  
+  everySecond: Observable<any> = timer(0, 1000);
+  
+  constructor(private ref: ChangeDetectorRef) {
+  this.searchEndDate = this.SearchDate.add(this.ElapsTime, "minutes");
+  }
+  
+  ngOnInit() {
+  this.subscription = this.everySecond.subscribe((seconds) => {
+  var currentTime: moment.Moment = moment();
+  this.remainingTime = this.searchEndDate.diff(currentTime)
+  this.remainingTime = this.remainingTime / 1000;
+  
+  if (this.remainingTime <= 0) {
+  this.SearchDate = moment();
+  this.searchEndDate = this.SearchDate.add(this.ElapsTime, "minutes");
+  
+  this.TimerExpired.emit();
+  }
+  else {
+  this.minutes = Math.floor(this.remainingTime / 60);
+  this.seconds = Math.floor(this.remainingTime - this.minutes * 60);
+          }
+  this.ref.markForCheck()
+  })
+  }
+  
+  ngOnDestroy(): void {
+  this.subscription.unsubscribe();
+  }
+  
+  }
